7f5fc7b0b6e9207afbe4248e2d4dfab087329de0
irvnet <irvnet@gmail.com>
2022-04-23 13:34:42 -0400

    fix:change cluster name

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'creating kind cluster... '
-kind create cluster --name consul --image kindest/node:v1.19.16 --config=../config/kind-cluster-config.yaml
+kind create cluster --name dev-cluster --image kindest/node:v1.19.16 --config=../config/kind-cluster-config.yaml
 
 
